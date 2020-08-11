c4d656713887f9fc37dce03acec8953a331256ba
Minh <nguyenminh@seznam.cz>
2020-08-11 19:11:28 +0200

    Add -Environments - spec file - Page object

--- cypress/integration/signIn.js
+++ cypress/integration/signIn.js
@@ -1,7 +1,10 @@
 import HomePage from '../support/PageObjects/HomePage'
 
-it('.type() - type into a DOM element', () => {
+it('Sign in with correct user details', () => {
     const home = new  HomePage();
     home.visit();
     const signIn = home.gotoSignIn();
+    signIn
+    .insertCredentials(Cypress.env('userName'),Cypress.env('password'))
+
 })
--- cypress/support/PageObjects/HomePage.js
+++ cypress/support/PageObjects/HomePage.js
@@ -1,18 +1,34 @@
 import Header from '../PageObjects/Header'
 
 class HomePage {
-constructor(){
-    this.Header = new Header();
-}
- visit(){
-     cy.visit('https://www.cypress.io')
- }
+    constructor(){
+        this.Header = new Header();
+    }
+    visit(){
+        cy.visit('https://www.alza.cz')
+    }
 
-gotoSignIn(){
-const link = this.Header.getSignInButton();
-link.click();
+    gotoSignIn(){
+        const link = this.Header.getSignInButton();
+        link.click();
+        return this
+    }
 
-}
+    insertCredentials(userName, password){
+        cy.get('#i_name')
+            .click()
+            .clear()
+            .type(userName)
+            .should('have.value', userName)
+        
+        cy.get('#i_psw')
+            .click()
+            .clear()
+            .type(password)
+            .should('have.value', password)
+        
+        return this;
+    }
 
 }
 export default HomePage;
